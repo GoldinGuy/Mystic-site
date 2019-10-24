cc582747a8f59b443dc16ec0df437b03e4ede059
Goldin Guy <sethgoldin27@gmail.com>
2019-10-23 21:41:17 -0400

    ui

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@ const TEMPLATE = $(function() {
 	let article_grid = $("#article-grid");
 
 	$.getJSON("https://mystic-api-test.herokuapp.com/articles", function(items) {
-		for (item of items) {
-			let new_item = `
+		let promises = items.map(item => {
+			return new Promise(resolve => {
+				if (item.image_url) {
+					let new_item = `
 <a class="card" href="${item.url}">
   <div class="image art-height">
-    <img src="${item.image_url ? item.image_url : alt_img_url()} " />
+    <img src="${item.image_url}" />
   </div >
   <div class="content">
     <div class="header">${item.title}</div>
@@ -20,30 +22,82 @@ const TEMPLATE = $(function() {
     </span>
   </div>
 </div >`;
-			article_grid.append($(new_item));
-		}
+					resolve($(new_item));
+				} else {
+					$.getJSON("https://api.scryfall.com/cards/random").then(card => {
+						let new_item = `
+<a class="card" href="${item.url}">
+  <div class="image art-height">
+    <img src="${card.image_uris.art_crop} " />
+  </div >
+  <div class="content">
+    <div class="header">${item.title}</div>
+  </div>
+  <div class="extra content">
+    <span class="right floated">
+      ${moment.duration(moment(item.date).diff(new moment())).humanize()} ago
+      </span>
+    <span>
+      ${item.site_name}
+    </span>
+  </div>
+</div >`;
+						resolve($(new_item));
+					});
+				}
+			});
+		});
+
+		Promise.all(promises).then(cards => article_grid.append(...cards));
 	});
 });
 
-function getRandomArt(count) {
-	return Promise.all(
-		Array(count)
-			.fill("https://api.scryfall.com/cards/random")
-			.map($.getJSON)
-	).then(data => data.map(c => c.image_uris.art_crop));
-}
+// const TEMPLATE = $(function() {
+// 	let article_grid = $("#article-grid");
 
-function alt_img_url() {
-	// $.getJSON("https://api.scryfall.com/cards/random"),
-	// 	function(items) {
-	// 		for (item of items) {
-	// 			let art_crop = "${item.image_uris.art_crop}";
-	// 			return art_crop;
-	// 		}
-	// 	};
+// 	$.getJSON("https://mystic-api-test.herokuapp.com/articles", function(items) {
+// 		for (item of items) {
+// 			let new_item = `
+// <a class="card" href="${item.url}">
+//   <div class="image art-height">
+//     <img src="${item.image_url ? item.image_url : alt_img_url()} " />
+//   </div >
+//   <div class="content">
+//     <div class="header">${item.title}</div>
+//   </div>
+//   <div class="extra content">
+//     <span class="right floated">
+//       ${moment.duration(moment(item.date).diff(new moment())).humanize()} ago
+//       </span>
+//     <span>
+//       ${item.site_name}
+//     </span>
+//   </div>
+// </div >`;
+// 			article_grid.append($(new_item));
+// 		}
+// 	});
+// });
 
-	return getRandomArt(1);
-}
+// function getRandomArt(count) {
+// 	return Promise.all(
+// 		Array(count)
+// 			.fill("https://api.scryfall.com/cards/random")
+// 			.map($.getJSON)
+// 	).then(data => data.map(c => c.image_uris.art_crop));
+// }
+
+// function alt_img_url() {
+// $.getJSON("https://api.scryfall.com/cards/random"),
+// 	function(items) {
+// 		for (item of items) {
+// 			let art_crop = "${item.image_uris.art_crop}";
+// 			return art_crop;
+// 		}
+// 	};
+// }
+
+/* <img src="${item.image_url ? item.image_url : alt_img_url()} " /> */
 
 // item.img = card.art_crop;
 // if (item.image_url != null || item.image_url == '') {
